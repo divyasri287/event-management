7e37c4485788fa25f3c1b212dd72e35a3ccabebb
divyasri287 <divyasrip287@gmail.com>
2026-01-08 10:54:15 +0530

    Updated admin, user, events, signup

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let registrations = [];
+
+function register(){
+  let n = uname.value;
+
+  if(n==""){
+    alert("Enter your name");
+    return;
+  }
+
+  registrations.push(n);
+
+  let li = document.createElement("li");
+  li.innerText = n;
+  document.getElementById("list").appendChild(li);
+
+  uname.value = "";
+}
 
